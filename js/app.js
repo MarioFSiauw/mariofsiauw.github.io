ac043a53a7e27641066094420901c1b3cf322bc8
Mario Siauw <mario@collide.com>
2017-10-31 15:06:48 -0700

    pushing them pixels

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 $(document).foundation()
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
